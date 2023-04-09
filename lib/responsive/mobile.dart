f48d65e02cfb9e02208a4a58bdf4332ec08cece1
afandy72 <youssefafandy142@gmail.com>
2023-04-09 12:44:36 +0200

    6-Statfull Widget

---
+++ lib/responsive/mobile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MobileScreen extends StatefulWidget {
+  const MobileScreen({super.key});
+
+  @override
+  State<MobileScreen> createState() => _WebScreenState();
+}
+
+class _WebScreenState extends State<MobileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/responsive/web.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WebScreen extends StatefulWidget {
+  const WebScreen({super.key});
+
+  @override
+  State<WebScreen> createState() => _WebScreenState();
+}
+
+class _WebScreenState extends State<WebScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
