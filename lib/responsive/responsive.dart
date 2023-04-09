a51e4fcaa561d63d59b6fabd106d837278b197db
afandy72 <youssefafandy142@gmail.com>
2023-04-09 14:38:46 +0200

    9

---
+++ lib/responsive/responsive.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/responsive/mobile.dart';
+
+class Responsive extends StatefulWidget {
+  final mymobile;
+  final myweb;
+  const Responsive({super.key, required this.mymobile, required this.myweb});
+
+  @override
+  State<Responsive> createState() => _ResponsiveState();
+}
+
+class _ResponsiveState extends State<Responsive> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (BuildContext, BoxConstraints) {
+      if (BoxConstraints.maxWidth > 600) {
+        return MobileScreen();
+      } else {}
+    });
+  }
+}
