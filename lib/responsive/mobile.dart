25941eeb5d56880fdbf2f4469c8baeb3c96d49f6
afandy72 <youssefafandy142@gmail.com>
2023-04-23 13:44:13 +0200

    12

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -6,10 +6,10 @@ class MobileScreen extends StatefulWidget {
   const MobileScreen({super.key});
 
   @override
-  State<MobileScreen> createState() => _WebScreenState();
+  State<MobileScreen> createState() => _MobileScreen();
 }
 
-class _WebScreenState extends State<MobileScreen> {
+class _MobileScreen extends State<MobileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
