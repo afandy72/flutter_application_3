14cd11e6667353b733e418813e21673256cf457f
afandy72 <youssefafandy142@gmail.com>
2023-04-23 15:04:54 +0200

    25

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_application_3/Screens/add_photo.dart';
+import 'package:flutter_application_3/Screens/home.dart';
+import 'package:flutter_application_3/Screens/profile.dart';
+import 'package:flutter_application_3/Screens/search.dart';
 
 class MobileScreen extends StatefulWidget {
   const MobileScreen({super.key});
@@ -21,16 +25,46 @@ class _MobileScreen extends State<MobileScreen> {
           backgroundColor: Colors.black,
           onTap: (index) {},
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: ""),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
             BottomNavigationBarItem(
-                icon: Icon(Icons.add_box_outlined), label: ""),
-            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+                icon: Icon(
+                  Icons.home_rounded,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                label: ""),
             BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline), label: ""),
+                icon: Icon(
+                  Icons.search,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_box_outlined,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                label: ""),
           ]),
       body: PageView(
         onPageChanged: (index) {},
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          Home(),
+          Search(),
+          AddPost(),
+          Profile(),
+        ],
       ),
     );
 
