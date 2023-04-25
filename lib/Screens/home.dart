cbc0447096f8910093a55ae87a51d8d7d413ee62
afandy72 <youssefafandy142@gmail.com>
2023-04-25 23:43:01 +0200

    36

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,7 +13,14 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Screen"),
+        title: Text(
+          "Instagram",
+          style: GoogleFonts.lobster(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
