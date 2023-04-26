c89b7bf383d57b919d467d9246c8044b65466d80
afandy72 <youssefafandy142@gmail.com>
2023-04-27 01:03:07 +0200

    58

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -172,7 +172,7 @@ class _HomeState extends State<Home> {
             ],
           ),
           Padding(
-            padding: const EdgeInsets.only(right: 200.0),
+            padding: const EdgeInsets.all(8.0),
             child: Text("10 like"),
           )
         ],
