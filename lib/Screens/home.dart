557b3f17d6bc3b12cab95eec2c9763edd4c2e014
afandy72 <youssefafandy142@gmail.com>
2023-04-26 11:45:05 +0200

    45

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -18,10 +18,20 @@ class _HomeState extends State<Home> {
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
           Padding(
-            padding: const EdgeInsets.only(right: 12),
-            child: ImageIcon(AssetImage(
-                "assets/img/socialmedia-social-media-logo-facebook-messenger-icon-329837-256.png")),
-          )
+              padding: const EdgeInsets.only(top: 16, right: 15),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 15,
+                    width: 15,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  ImageIcon(AssetImage(
+                      "assets/img/socialmedia-social-media-logo-facebook-messenger-icon-329837-256.png")),
+                ],
+              ))
         ],
         backgroundColor: Colors.black,
         title: SvgPicture.asset(
