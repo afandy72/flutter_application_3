b12f292d03a16e7e3f107ff1b80dfa4cf44352e2
afandy72 <youssefafandy142@gmail.com>
2023-04-26 12:05:30 +0200

    47

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -13,42 +13,46 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
-          Padding(
-              padding: const EdgeInsets.only(top: 16, right: 15),
-              child: Stack(
-                children: [
-                  Container(
-                    child: Text(
-                      "4",
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    padding: EdgeInsets.only(
-                      left: 4,
-                    ),
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                        color: Colors.red, shape: BoxShape.circle),
-                  ),
-                  ImageIcon(AssetImage(
-                      "assets/img/socialmedia-social-media-logo-facebook-messenger-icon-329837-256.png")),
-                ],
-              ))
-        ],
         backgroundColor: Colors.black,
-        title: SvgPicture.asset(
-          "assets/img/Instagram_logo.svg",
-          width: 150,
-          color: Colors.white,
-        ),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: SvgPicture.asset(
+            "assets/img/Instagram_logo.svg",
+            width: 150,
+            color: Colors.white,
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: Text(
+                          "4",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        padding: EdgeInsets.all(
+                          4,
+                        ),
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                            color: Colors.red, shape: BoxShape.circle),
+                      ),
+                      ImageIcon(AssetImage(
+                          "assets/img/socialmedia-social-media-logo-facebook-messenger-icon-329837-256.png")),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
