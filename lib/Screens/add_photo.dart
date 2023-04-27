e9ab8b569287572139e6b93e60273dfb43ecd47e
afandy72 <youssefafandy142@gmail.com>
2023-04-27 02:33:59 +0200

    103

--- lib/Screens/add_photo.dart
+++ lib/Screens/add_photo.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class AddPost extends StatefulWidget {
+  const AddPost({super.key});
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("AddPost Screen"),
+      ),
+    );
+  }
+}
